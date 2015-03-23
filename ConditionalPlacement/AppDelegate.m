14d95caea9d4e4a9e94a429a736472d7e05e67a1
Dan Clawson <clawson@uen.org>
2015-03-23 10:21:56 -0600

    Add placement check for Karl and Leonard

--- ConditionalPlacement/AppDelegate.m
+++ ConditionalPlacement/AppDelegate.m
@@ -16,7 +16,24 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    int karl = 5;
+    int leonard = 8;
+    
+    int karlPlace = karl > 7 ? 1 : 2;
+    int leonardPlace = leonard > 7 ? 1 : 2;
+        
+    if (karlPlace == 1 && leonardPlace == 1) {
+        NSLog(@"Both teachers are placed 1");
+    } else if (karlPlace == 1  && leonardPlace == 2) {
+        NSLog(@"Karl is placed 1 and Leonard is placed 2");
+    } else if (leonardPlace == 1 && karlPlace == 2) {
+        NSLog(@"Leonard is placed 1 and Karl is placed 2");
+    } else {
+        NSLog(@"Karl and Leonard are placed 2");
+    }
+    
+    
     return YES;
 }
 
